ce1198bcc1694132ab6b9baaf7ba4206678ac4ee
Katarzyna Brewinska <brewinska.katarzyna@gmail.com>
2020-02-17 16:14:21 +0100

    next String exercises added and Bot Chatty added

---
+++ Problems (34)/Extracting a substring/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputWord = scanner.nextLine();
+        Scanner scanner1 = new Scanner(System.in);
+        int firstIndex = scanner1.nextInt();
+        int secondIndex = scanner1.nextInt();
+        String result = inputWord.substring(firstIndex, secondIndex + 1);
+        System.out.println(result);
+    }
+}
